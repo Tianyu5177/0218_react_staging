dc27cd2ec553795397c3b6bdbf02b333e5ae249d
peiqi <peiqi@sina.com>
2019-06-10 11:54:13 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,69 @@
 import React,{Component} from 'react'
-import Add from './components/add/Add'
-import List from './components/list/List'
+import axios from 'axios'
 
 export default class App extends Component{
 
-  state = {
-    comments:[
-      {id:'x89vcuboucxiuyboi7',userName:'王帅',content:'react just soso!'},
-      {id:'siaudyf876sd98f7s8',userName:'丽君',content:'我觉得很容易'},
-      {id:'xcv78xv987x9vx987x',userName:'班长',content:'等我节后归来'}
-    ]
+  state =  {
+    repoName:'',//仓库名
+    repoUrl:'',//仓库地址
+    loading:true,//标识是否正在加载
+    errorMsg:''//错误信息
   }
 
-  addComment =(commentObj)=>{
-    //1.获取原状态----获取原状态时，不要用解构赋值（数据不是基本类型的时）。即：不要直接用原来的数据，要“缔造”一个新数据。
-    //let {comments} = this.state
-    let comments = [...this.state.comments]
-    //console.log(comments === this.state.comments)
+  async componentDidMount(){
+    console.log('组件已经挂载到页面上了！开始发送请求')
 
-    /*2.修改状态（不能直接修改,要三点运算符）
-      直接修改----如果使用了解构赋值方式获取到的state里的具体数据，那么如下代码就属于直接修改状态。
-      非直接修改----使用了三点运算符，获取到原状态（实质上产生了一个新的对象）。（要用这种方式）*/
-    comments.unshift(commentObj)
+    //发送请求
+    const URL = `https://api.github.com/search/repositories?q=r&sort=stars`
+    try{
+      let response =  await axios.get(URL)
+      let data = response.data.items[0]
+      this.setState({
+        repoName:data.name,//仓库名
+        repoUrl:data.html_url,//仓库地址
+        loading:false,//标识是否正在加载
+        errorMsg:''//错误信息
+      })
+    }
+    catch(err){
+      this.setState({
+        repoName:'',//仓库名
+        repoUrl:'',//仓库地址
+        loading:false,//标识是否正在加载
+        errorMsg:err.message//错误信息
+      })
+    }
+    /*axios.get(URL)
+      .then((response)=>{
+        console.log(response.data.items[0])
+        let data = response.data.items[0]
+        this.setState({
+          repoName:data.name,//仓库名
+          repoUrl:data.html_url,//仓库地址
+          loading:false,//标识是否正在加载
+          errorMsg:''//错误信息
+        })
+      })
+      .catch((err)=>{
+        console.log(err.message)
+        this.setState({
+          repoName:'',//仓库名
+          repoUrl:'',//仓库地址
+          loading:false,//标识是否正在加载
+          errorMsg:err.message//错误信息
+        })
+      })*/
 
-    //3.更新状态（不能直接更新，要用this.setState方法）
-    //this.setState.comments = {}
-    this.setState({comments})
-  }
-
-  deleteComment =(id)=>{
-    let comments = [...this.state.comments]
-    comments.forEach((item,index)=>{
-      if(item.id === id){
-        //删掉
-        comments.splice(index,1)
-      }
-    })
-    this.setState({comments})
   }
 
   render(){
-    let {comments} = this.state
-    return (
-      <div id="app">
-        <div>
-          <header className="site-header jumbotron">
-            <div className="container">
-              <div className="row">
-                <div className="col-xs-12">
-                  <h1>请发表对React的评论</h1>
-                </div>
-              </div>
-            </div>
-          </header>
-          <div className="container">
-            <Add addComment={this.addComment}/>
-            <List comments={comments} deleteComment={this.deleteComment}/>
-          </div>
-        </div>
-      </div>
-    )
+    let {repoName,repoUrl,loading,errorMsg} = this.state
+    if(loading){
+      return <h2>loading...</h2>
+    }else if(repoName && repoUrl){
+      return <h2>github上点赞最多的仓库是：<a href={repoUrl}>{repoName}</a></h2>
+    }else if(errorMsg){
+      return <h2 style={{color:'red'}}>{errorMsg}</h2>
+    }
   }
 }
