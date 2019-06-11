77cd91742d1decd47ad01fe3f4882f16de6fbe41
peiqi <peiqi@sina.com>
2019-06-11 12:01:22 +0800

    路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
 import React,{Component} from 'react'
-import Search from './components/search/Search'
-import List from './components/list/List'
+import {NavLink,Route,Redirect,Switch} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
 
 export default class App extends Component{
   render(){
     return (
-      <div className="container">
-        <Search/>
-        <List/>
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <h2>React Router Demo</h2>
+              <input type="text"/>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/*路由跳转*/}
+              <NavLink className="list-group-item" activeClassName="demo" to="/about">About</NavLink>
+              <NavLink className="list-group-item" activeClassName="demo" to="/home">Home</NavLink>
+
+              {/*如下代码是普通传统按标签跳转，页面刷新，需要有多个页面配合*/}
+              {/*<a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a>*/}
+
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/*注册路由*/}
+                <Switch>
+                  <Route path="/about" component={About}/>
+                  <Route path="/home" component={Home}/>
+                  <Redirect to="/about"/>
+                </Switch>
+
+                {/*要展示的不同内容*/}
+                {/*<h3>我是About的内容</h3>
+                <h3>我是Home的内容</h3>*/}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {HashRouter,BrowserRouter} from 'react-router-dom'
 import App from './App'
 
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+
+ReactDOM.render(
+  <HashRouter>
+    <App/>
+  </HashRouter>,
+  document.getElementById('root'))
